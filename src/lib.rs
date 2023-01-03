f2b26ba60fa32fbf5c6d48ade8ed8fde5609b0d3
Jack Lund <jackl@geekheads.net>
2023-01-03 10:11:23 -0600

    Challenge 6

--- src/lib.rs
+++ src/lib.rs
@@ -312,6 +312,76 @@ pub fn repeating_key_xor(key: &[u8], plaintext: &[u8]) -> Vec<u8> {
         .collect()
 }
 
+pub fn hamming_distance(a: &[u8], b: &[u8]) -> usize {
+    a.iter()
+        .zip(b)
+        .fold(0u32, |sum, (a, b)| sum + (a ^ b).count_ones()) as usize
+}
+
+pub fn break_repeating_key_xor(ciphertext: &[u8]) -> (Vec<u8>, Vec<u8>) {
+    // Iterate through various keysizes, chunking the ciphertext into keysize chunks,
+    // and find the least average distance between the even and odd chunks
+    // That will determine our key size
+    let (_distance, keysize) = (2..40).fold(
+        (1000f32, 1usize),
+        |(last_distance, last_keysize), keysize| {
+            // Chunk the ciphertext
+            let (odd, even): (Vec<(usize, &[u8])>, Vec<(usize, &[u8])>) = ciphertext
+                .chunks(keysize)
+                .enumerate()
+                .partition(|(i, _chunk)| i % 2 == 0);
+
+            // Compute the average distance
+            // This is average distance divided by key length.
+            // Since average distance = total distance / number of samples,
+            // and number of samples = string length / key length,
+            // we end up with average distance = total distance / string length
+            let average_distance = odd
+                .iter()
+                .zip(even)
+                .map(|((_, odd_chunk), (_, even_chunk))| hamming_distance(odd_chunk, even_chunk))
+                .sum::<usize>() as f32
+                / ciphertext.len() as f32;
+            if average_distance < last_distance {
+                (average_distance, keysize)
+            } else {
+                (last_distance, last_keysize)
+            }
+        },
+    );
+
+    // Now that we have the key size, chunk the ciphertext in keysize chunks, and then transpose it
+    // by taking the first byte of each chunk, then the second, etc. This leaves us with a bunch of
+    // single-byte-encrypted ciphertexts.
+    let mut nested_vecs: Vec<Vec<u8>> = Vec::new();
+    (0..keysize).for_each(|_| nested_vecs.push(Vec::new()));
+    let nested_vecs = ciphertext
+        .chunks(keysize)
+        .fold(nested_vecs, |mut nested_vecs, chunk| {
+            chunk
+                .iter()
+                .enumerate()
+                .for_each(|(i, b)| nested_vecs[i].push(*b));
+
+            nested_vecs
+        });
+
+    // We find the single-byte key for each transposed ciphertext, and combine each of those into
+    // our overall key
+    let key = nested_vecs
+        .iter()
+        .map(|ciphertext| {
+            let (key, _score, _plaintext) = find_single_byte_key(ciphertext);
+            key
+        })
+        .collect::<Vec<u8>>();
+
+    // ...and we decrypt
+    let plaintext = repeating_key_xor(&key, ciphertext);
+
+    (key, plaintext)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -329,6 +399,14 @@ mod tests {
         assert_eq!(
             to_base64("Many hands make light work.".as_bytes()),
             "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"
+        );
+    }
+
+    #[test]
+    fn test_hamming() {
+        assert_eq!(
+            37,
+            hamming_distance("this is a test".as_bytes(), "wokka wokka!!!".as_bytes()),
         );
     }
 }
