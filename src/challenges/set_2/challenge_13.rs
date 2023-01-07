6b59525b505cb0db2dfc8ff0728abc0e47ae911e
Jack Lund <jackl@geekheads.net>
2023-01-06 22:16:32 -0600

    Challenge 13

--- src/challenges/set_2/challenge_13.rs
+++ src/challenges/set_2/challenge_13.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::aes::{ecb_decrypt, ecb_encrypt, find_blocksize, generate_key};
-    use crate::util::pkcs7_pad;
+    use crate::util::{get_padding_size, pkcs7_pad};
     use std::collections::HashMap;
 
     fn profile_for(email: &str) -> String {
@@ -12,6 +12,7 @@ mod tests {
     fn to_hashmap(profile: &str) -> HashMap<String, String> {
         let kv_list = profile
             .split("&")
+            .take(3)
             .map(|kv| kv.split("=").collect::<Vec<&str>>())
             .map(|v| (v[0].to_string(), v[1].to_string()));
         HashMap::from_iter(kv_list)
@@ -19,6 +20,8 @@ mod tests {
 
     #[test]
     fn challenge_13() {
+        const PREFIX: usize = "email=".len();
+        const SUFFIX: usize = "&uid=10&role=".len();
         const BLOCKSIZE: usize = 16;
         let key = generate_key(BLOCKSIZE);
 
@@ -30,7 +33,8 @@ mod tests {
             )
         };
         let decrypt_profile = |ciphertext: &[u8]| {
-            to_hashmap(&std::str::from_utf8(&ecb_decrypt(&key, ciphertext, BLOCKSIZE)).unwrap())
+            let decrypted = ecb_decrypt(&key, ciphertext, BLOCKSIZE);
+            String::from_utf8(decrypted).unwrap()
         };
 
         let blocksize = find_blocksize(&|plaintext| {
@@ -38,5 +42,28 @@ mod tests {
         })
         .unwrap();
         assert_eq!(16, blocksize);
+
+        // Create a fake email which pushes the "admin" part past a block boundary
+        let mut fake_email = std::iter::repeat('A')
+            .take(blocksize - PREFIX)
+            .collect::<String>();
+        fake_email.push_str("admin");
+        let encrypted = encrypt_profile_for(&fake_email);
+
+        // Grab the block containing "admin..."
+        let admin_part = &encrypted[blocksize..2 * blocksize];
+
+        // Now create an email which pushes the "role=" just at a block boundary
+        let padding = get_padding_size(PREFIX + SUFFIX, blocksize);
+        let fake_email = std::iter::repeat('A').take(padding).collect::<String>();
+        let mut encrypted = encrypt_profile_for(&fake_email);
+
+        // Truncate the last block and add our admin block
+        encrypted.resize(encrypted.len() - blocksize, 0u8);
+        encrypted.extend(admin_part);
+
+        // Decrypt and test
+        let decrypted = decrypt_profile(&encrypted);
+        assert_eq!("admin", to_hashmap(&decrypted).get("role").unwrap());
     }
 }
