4e55bac58e76269453e3e3f47fbe27a795fc9ccb
Jack Lund <jackl@geekheads.net>
2023-01-26 19:19:48 -0600

    Challenge 32

--- src/challenges/set_4/challenge_32.rs
+++ src/challenges/set_4/challenge_32.rs
@@ -3,23 +3,27 @@ mod tests {
     use crate::digest::sha1::SHA1;
     use crate::hmac::*;
 
+    // This was a bear. I tried a huge variety of approaches, starting with the one that worked
+    // with 31, zscores, but that didn't work here, nor did using means, max/min, nothing. The
+    // problem is that there's just too much noisy jitter in the measurements, so that by the time
+    // we get halfway through the sequence, you can't distinguish a real from a false positive.
     #[ignore]
     #[test]
-    fn challenge31() {
+    fn challenge32() {
         let key = "Yellow Submarine";
         let real_signature = hmac_sha1(key.as_bytes(), "foo".as_bytes());
-        let mut server = HMacServer::new(50, true).start().unwrap();
+        let mut server = HMacServer::new(5, true).start().unwrap();
 
         let test_fn = |plaintext: &[u8], signature: &[u8]| {
             let status = tokio_test::block_on(async {
                 hmac_client(std::str::from_utf8(plaintext).unwrap(), signature).await
             });
             if status != reqwest::StatusCode::INTERNAL_SERVER_ERROR {
-                println!("Status = {:?}", status);
+                println!("Status = {:?}", status)
             }
             status == reqwest::StatusCode::OK
         };
-        let signature = hmac_timing_attack::<SHA1, _>(&test_fn, "foo".as_bytes(), 3.0);
+        let signature = hmac_timing_attack::<SHA1, _>(&test_fn, "foo".as_bytes(), 1.0);
         server.close().unwrap();
         assert!(signature.is_some());
         assert_eq!(real_signature, signature.unwrap());
--- src/challenges/set_4/mod.rs
+++ src/challenges/set_4/mod.rs
@@ -5,3 +5,4 @@ mod challenge_28;
 mod challenge_29;
 mod challenge_30;
 mod challenge_31;
+mod challenge_32;
