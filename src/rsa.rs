074cf98e50b0786a8c7f309720290a47a3086d74
Jack Lund <jackl@geekheads.net>
2023-02-05 22:34:45 -0600

    Speed up (my) RSA algorithm
    I originally used gen_safe_prime() to generate the p and q values,
    because not using it caused d to be None a good bit of the time, but
    that was really slow. Instead, I'm using gen_prime, and just retrying if
    d comes out as None. Also, changed the key modulus and exponent to be
    BigUint rather than BigInt.

--- src/rsa.rs
+++ src/rsa.rs
@@ -6,15 +6,15 @@ use rand;
 
 #[derive(Debug)]
 pub struct RSAKey {
-    pub modulus: BigInt,
-    pub exponent: BigInt,
+    pub modulus: BigUint,
+    pub exponent: BigUint,
 }
 
 type RSAPublicKey = RSAKey;
 type RSAPrivateKey = RSAKey;
 
 impl RSAKey {
-    pub fn new(modulus: &BigInt, exponent: &BigInt) -> Self {
+    pub fn new(modulus: &BigUint, exponent: &BigUint) -> Self {
         Self {
             modulus: modulus.clone(),
             exponent: exponent.clone(),
@@ -24,32 +24,37 @@ impl RSAKey {
 
 // Generate a keypair
 pub fn generate_keypair(bit_size: usize) -> (RSAPrivateKey, RSAPublicKey) {
-    let p_tmp: BigUint = rand::thread_rng().gen_safe_prime(bit_size);
-    let q_tmp: BigUint = rand::thread_rng().gen_safe_prime(bit_size);
-    let p: BigInt = p_tmp.to_bigint().unwrap();
-    let q: BigInt = q_tmp.to_bigint().unwrap();
+    // We loop until we get a p, q pair that will let us generate our d value correctly
+    // Tried using "gen_safe_prime()", which worked, but was really slow. This speeds it up
+    // considerably
+    loop {
+        let p: BigUint = rand::thread_rng().gen_prime(bit_size, None);
+        let q: BigUint = rand::thread_rng().gen_prime(bit_size, None);
 
-    let n = p.clone() * q.clone();
-    let et = (p - BigInt::one()) * (q - BigInt::one());
-    let e = BigUint::from(3u32);
-    let d: BigInt = e.clone().invm(&et.to_biguint().unwrap()).unwrap().into();
+        let n = p.clone() * q.clone();
+        let et = (p - BigUint::one()) * (q - BigUint::one());
+        let e = BigUint::from(3u32);
+        let d: Option<BigUint> = e.clone().invm(&et);
 
-    (RSAKey::new(&n, &d), RSAKey::new(&n, &e.into()))
+        if d.is_none() {
+            continue;
+        }
+
+        return (RSAKey::new(&n, &d.unwrap()), RSAKey::new(&n, &e.into()));
+    }
 }
 
 pub fn encrypt(key: &RSAPublicKey, plaintext: &[u8]) -> Vec<u8> {
-    BigInt::from_bytes_le(Sign::Plus, plaintext)
+    BigUint::from_bytes_le(plaintext)
         .modpow(&key.exponent, &key.modulus)
         .to_bytes_le()
-        .1
         .to_vec()
 }
 
 pub fn decrypt(key: &RSAPrivateKey, ciphertext: &[u8]) -> Vec<u8> {
-    BigInt::from_bytes_le(Sign::Plus, ciphertext)
+    BigUint::from_bytes_le(ciphertext)
         .modpow(&key.exponent, &key.modulus)
         .to_bytes_le()
-        .1
         .to_vec()
 }
 
