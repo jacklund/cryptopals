9f9e8731e25d4ae8a65488f2a7b9c0fdba3742fe
Jack Lund <jackl@geekheads.net>
2023-02-22 00:17:36 -0600

    Comments for CTR

--- src/ctr.rs
+++ src/ctr.rs
@@ -1,6 +1,7 @@
 use crate::ecb::ecb_encrypt_without_padding;
 use std::collections::VecDeque;
 
+// Iterator to generate a key stream
 pub struct KeyStreamIterator {
     counter: u64,
     key: Vec<u8>,
@@ -10,6 +11,7 @@ pub struct KeyStreamIterator {
 }
 
 impl KeyStreamIterator {
+    // Generate a keystream from the given key, and nonce
     pub fn new(key: &[u8], nonce: u64, blocksize: usize) -> Self {
         Self {
             counter: 0,
@@ -21,9 +23,12 @@ impl KeyStreamIterator {
     }
 
     pub fn next_block(&mut self) {
+        // Nonce || a counter
         let mut nonce_and_counter = self.nonce.to_le_bytes().to_vec();
         nonce_and_counter.extend(self.counter.to_le_bytes().to_vec());
         self.counter += 1;
+
+        // Generate the key block by encrypting the nonce-and-counter with the key
         self.key_block = VecDeque::from(ecb_encrypt_without_padding(
             &self.key,
             &nonce_and_counter,
@@ -43,6 +48,8 @@ impl Iterator for KeyStreamIterator {
     }
 }
 
+// Once we have the keystream set up, this part is easy-peazy
+// Zip the keystream with the input, xor them, and Bob's your uncle
 pub fn ctr(key: &[u8], nonce: u64, input: &[u8], blocksize: usize) -> Vec<u8> {
     KeyStreamIterator::new(key, nonce, blocksize)
         .zip(input.iter())
