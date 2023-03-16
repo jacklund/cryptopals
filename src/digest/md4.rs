3ceb13e6a7e512c394d63c5625567a8cf918216b
Jack Lund <jackl@geekheads.net>
2023-03-15 23:32:45 -0500

    Refactored MD4

--- src/digest/md4.rs
+++ src/digest/md4.rs
@@ -3,20 +3,22 @@ use asn1::{oid, ObjectIdentifier};
 
 const MD4_OID: ObjectIdentifier = oid!(1, 2, 840, 113549, 2, 3);
 
+// Default initialization constants
 const A: u32 = u32::from_be(0x01234567);
 const B: u32 = u32::from_be(0x89abcdef);
 const C: u32 = u32::from_be(0xfedcba98);
 const D: u32 = u32::from_be(0x76543210);
 
-fn f(x: u32, y: u32, z: u32) -> u32 {
+// Auxillary functions used to generate the hashes
+fn f(x: &u32, y: &u32, z: &u32) -> u32 {
     (x & y) | (!x & z)
 }
 
-fn g(x: u32, y: u32, z: u32) -> u32 {
+fn g(x: &u32, y: &u32, z: &u32) -> u32 {
     (x & y) | (x & z) | (y & z)
 }
 
-fn h(x: u32, y: u32, z: u32) -> u32 {
+fn h(x: &u32, y: &u32, z: &u32) -> u32 {
     x ^ y ^ z
 }
 
@@ -27,6 +29,7 @@ fn get_padding_size(message_len: usize) -> usize {
     }
 }
 
+/// Return the padding for the given message length
 pub fn get_padding(message_len: usize) -> Vec<u8> {
     let padding_size = get_padding_size(message_len);
 
@@ -40,128 +43,162 @@ pub fn get_padding(message_len: usize) -> Vec<u8> {
     padding
 }
 
+/// Convenience function to generate an MD4 hash of the given message
 pub fn md4(message: &[u8]) -> Vec<u8> {
     MD4::new().update(message).digest()
 }
 
+/// Convenience function to generate an MD4 MAC of the given message with the given key
 pub fn md4_mac(key: &[u8], message: &[u8]) -> Vec<u8> {
     MD4::new().mac(key, message)
 }
 
+/// MD trait, generates the basic round operations for an MD hash
+pub trait MD {
+    fn operation_1(&mut self, index: usize, value: u32, shift: u32);
+    fn operation_2(&mut self, index: usize, value: u32, shift: u32);
+    fn operation_3(&mut self, index: usize, value: u32, shift: u32);
+
+    fn round_1(&mut self, block: &[u32]) {
+        self.operation_1(0, block[0], 3);
+        self.operation_1(3, block[1], 7);
+        self.operation_1(2, block[2], 11);
+        self.operation_1(1, block[3], 19);
+        self.operation_1(0, block[4], 3);
+        self.operation_1(3, block[5], 7);
+        self.operation_1(2, block[6], 11);
+        self.operation_1(1, block[7], 19);
+        self.operation_1(0, block[8], 3);
+        self.operation_1(3, block[9], 7);
+        self.operation_1(2, block[10], 11);
+        self.operation_1(1, block[11], 19);
+        self.operation_1(0, block[12], 3);
+        self.operation_1(3, block[13], 7);
+        self.operation_1(2, block[14], 11);
+        self.operation_1(1, block[15], 19);
+    }
+
+    fn round_2(&mut self, block: &[u32]) {
+        self.operation_2(0, block[0], 3);
+        self.operation_2(3, block[4], 5);
+        self.operation_2(2, block[8], 9);
+        self.operation_2(1, block[12], 13);
+        self.operation_2(0, block[1], 3);
+        self.operation_2(3, block[5], 5);
+        self.operation_2(2, block[9], 9);
+        self.operation_2(1, block[13], 13);
+        self.operation_2(0, block[2], 3);
+        self.operation_2(3, block[6], 5);
+        self.operation_2(2, block[10], 9);
+        self.operation_2(1, block[14], 13);
+        self.operation_2(0, block[3], 3);
+        self.operation_2(3, block[7], 5);
+        self.operation_2(2, block[11], 9);
+        self.operation_2(1, block[15], 13);
+    }
+
+    fn round_3(&mut self, block: &[u32]) {
+        self.operation_3(0, block[0], 3);
+        self.operation_3(3, block[8], 9);
+        self.operation_3(2, block[4], 11);
+        self.operation_3(1, block[12], 15);
+        self.operation_3(0, block[2], 3);
+        self.operation_3(3, block[10], 9);
+        self.operation_3(2, block[6], 11);
+        self.operation_3(1, block[14], 15);
+        self.operation_3(0, block[1], 3);
+        self.operation_3(3, block[9], 9);
+        self.operation_3(2, block[5], 11);
+        self.operation_3(1, block[13], 15);
+        self.operation_3(0, block[3], 3);
+        self.operation_3(3, block[11], 9);
+        self.operation_3(2, block[7], 11);
+        self.operation_3(1, block[15], 15);
+    }
+}
+
+/// MD4 hash
 pub struct MD4 {
-    a: u32,
-    b: u32,
-    c: u32,
-    d: u32,
-}
-
-fn operation_1(a: u32, b: u32, c: u32, d: u32, value: u32, shift: u32) -> u32 {
-    a.wrapping_add(f(b, c, d))
-        .wrapping_add(value)
-        .rotate_left(shift)
-}
-
-fn operation_2(a: u32, b: u32, c: u32, d: u32, value: u32, shift: u32) -> u32 {
-    a.wrapping_add(g(b, c, d))
-        .wrapping_add(value)
-        .wrapping_add(0x5a827999u32)
-        .rotate_left(shift)
-}
-
-fn operation_3(a: u32, b: u32, c: u32, d: u32, value: u32, shift: u32) -> u32 {
-    a.wrapping_add(h(b, c, d))
-        .wrapping_add(value)
-        .wrapping_add(0x6ed9eba1u32)
-        .rotate_left(shift)
+    state: [u32; 4],
 }
 
 impl MD4 {
+    /// Initialize MD4 with customized values
     pub fn new_with_init(a: u32, b: u32, c: u32, d: u32) -> Self {
-        Self { a, b, c, d }
+        Self {
+            state: [a, b, c, d],
+        }
     }
 
+    /// Generate an MD4 MAC
     pub fn mac(&mut self, key: &[u8], message: &[u8]) -> Vec<u8> {
         let mut value = key.to_vec();
         value.extend(message);
         self.update(&value).digest()
     }
+}
 
-    fn round_1(&mut self, block: &[u32]) {
-        self.a = operation_1(self.a, self.b, self.c, self.d, block[0], 3);
-        self.d = operation_1(self.d, self.a, self.b, self.c, block[1], 7);
-        self.c = operation_1(self.c, self.d, self.a, self.b, block[2], 11);
-        self.b = operation_1(self.b, self.c, self.d, self.a, block[3], 19);
-        self.a = operation_1(self.a, self.b, self.c, self.d, block[4], 3);
-        self.d = operation_1(self.d, self.a, self.b, self.c, block[5], 7);
-        self.c = operation_1(self.c, self.d, self.a, self.b, block[6], 11);
-        self.b = operation_1(self.b, self.c, self.d, self.a, block[7], 19);
-        self.a = operation_1(self.a, self.b, self.c, self.d, block[8], 3);
-        self.d = operation_1(self.d, self.a, self.b, self.c, block[9], 7);
-        self.c = operation_1(self.c, self.d, self.a, self.b, block[10], 11);
-        self.b = operation_1(self.b, self.c, self.d, self.a, block[11], 19);
-        self.a = operation_1(self.a, self.b, self.c, self.d, block[12], 3);
-        self.d = operation_1(self.d, self.a, self.b, self.c, block[13], 7);
-        self.c = operation_1(self.c, self.d, self.a, self.b, block[14], 11);
-        self.b = operation_1(self.b, self.c, self.d, self.a, block[15], 19);
+/// MD implementation for MD4
+impl MD for MD4 {
+    fn operation_1(&mut self, index: usize, value: u32, shift: u32) {
+        // Syntactic sugar
+        let a = &self.state[index];
+        let b = &self.state[(index + 1) % self.state.len()];
+        let c = &self.state[(index + 2) % self.state.len()];
+        let d = &self.state[(index + 3) % self.state.len()];
+
+        self.state[index] = a
+            .wrapping_add(f(b, c, d))
+            .wrapping_add(value)
+            .rotate_left(shift);
     }
 
-    fn round_2(&mut self, block: &[u32]) {
-        self.a = operation_2(self.a, self.b, self.c, self.d, block[0], 3);
-        self.d = operation_2(self.d, self.a, self.b, self.c, block[4], 5);
-        self.c = operation_2(self.c, self.d, self.a, self.b, block[8], 9);
-        self.b = operation_2(self.b, self.c, self.d, self.a, block[12], 13);
-        self.a = operation_2(self.a, self.b, self.c, self.d, block[1], 3);
-        self.d = operation_2(self.d, self.a, self.b, self.c, block[5], 5);
-        self.c = operation_2(self.c, self.d, self.a, self.b, block[9], 9);
-        self.b = operation_2(self.b, self.c, self.d, self.a, block[13], 13);
-        self.a = operation_2(self.a, self.b, self.c, self.d, block[2], 3);
-        self.d = operation_2(self.d, self.a, self.b, self.c, block[6], 5);
-        self.c = operation_2(self.c, self.d, self.a, self.b, block[10], 9);
-        self.b = operation_2(self.b, self.c, self.d, self.a, block[14], 13);
-        self.a = operation_2(self.a, self.b, self.c, self.d, block[3], 3);
-        self.d = operation_2(self.d, self.a, self.b, self.c, block[7], 5);
-        self.c = operation_2(self.c, self.d, self.a, self.b, block[11], 9);
-        self.b = operation_2(self.b, self.c, self.d, self.a, block[15], 13);
+    fn operation_2(&mut self, index: usize, value: u32, shift: u32) {
+        // Syntactic sugar
+        let a = &self.state[index];
+        let b = &self.state[(index + 1) % self.state.len()];
+        let c = &self.state[(index + 2) % self.state.len()];
+        let d = &self.state[(index + 3) % self.state.len()];
+
+        self.state[index] = a
+            .wrapping_add(g(b, c, d))
+            .wrapping_add(value)
+            .wrapping_add(0x5a827999u32)
+            .rotate_left(shift);
     }
 
-    fn round_3(&mut self, block: &[u32]) {
-        self.a = operation_3(self.a, self.b, self.c, self.d, block[0], 3);
-        self.d = operation_3(self.d, self.a, self.b, self.c, block[8], 9);
-        self.c = operation_3(self.c, self.d, self.a, self.b, block[4], 11);
-        self.b = operation_3(self.b, self.c, self.d, self.a, block[12], 15);
-        self.a = operation_3(self.a, self.b, self.c, self.d, block[2], 3);
-        self.d = operation_3(self.d, self.a, self.b, self.c, block[10], 9);
-        self.c = operation_3(self.c, self.d, self.a, self.b, block[6], 11);
-        self.b = operation_3(self.b, self.c, self.d, self.a, block[14], 15);
-        self.a = operation_3(self.a, self.b, self.c, self.d, block[1], 3);
-        self.d = operation_3(self.d, self.a, self.b, self.c, block[9], 9);
-        self.c = operation_3(self.c, self.d, self.a, self.b, block[5], 11);
-        self.b = operation_3(self.b, self.c, self.d, self.a, block[13], 15);
-        self.a = operation_3(self.a, self.b, self.c, self.d, block[3], 3);
-        self.d = operation_3(self.d, self.a, self.b, self.c, block[11], 9);
-        self.c = operation_3(self.c, self.d, self.a, self.b, block[7], 11);
-        self.b = operation_3(self.b, self.c, self.d, self.a, block[15], 15);
+    fn operation_3(&mut self, index: usize, value: u32, shift: u32) {
+        // Syntactic sugar
+        let a = &self.state[index];
+        let b = &self.state[(index + 1) % self.state.len()];
+        let c = &self.state[(index + 2) % self.state.len()];
+        let d = &self.state[(index + 3) % self.state.len()];
+
+        self.state[index] = a
+            .wrapping_add(h(b, c, d))
+            .wrapping_add(value)
+            .wrapping_add(0x6ed9eba1u32)
+            .rotate_left(shift);
     }
 }
 
+/// Digest trait implementation for MD4
 impl Digest for MD4 {
     const BLOCKSIZE: usize = 64;
     const OUTPUT_SIZE: usize = 16;
     const OID: ObjectIdentifier = MD4_OID;
 
+    /// Generate an MD4 struct with the default initial values
     fn new() -> Self {
-        Self {
-            a: A,
-            b: B,
-            c: C,
-            d: D,
-        }
+        Self::new_with_init(A, B, C, D)
     }
 
+    /// Update with a message
     fn update(&mut self, message: &[u8]) -> &mut Self {
         self.update_with_length(message, message.len())
     }
 
+    /// Update with a message of a given length
     fn update_with_length(&mut self, message: &[u8], message_len: usize) -> &mut Self {
         // Pad message out so that len % 64 = 56
         let mut output = message.to_vec();
@@ -180,22 +217,23 @@ impl Digest for MD4 {
             self.round_3(block);
 
             // Add the original value back
-            self.a = self.a.wrapping_add(A);
-            self.b = self.b.wrapping_add(B);
-            self.c = self.c.wrapping_add(C);
-            self.d = self.d.wrapping_add(D);
+            self.state[0] = self.state[0].wrapping_add(A);
+            self.state[1] = self.state[1].wrapping_add(B);
+            self.state[2] = self.state[2].wrapping_add(C);
+            self.state[3] = self.state[3].wrapping_add(D);
         }
 
         self
     }
 
+    /// Generate the digest
     fn digest(&mut self) -> Vec<u8> {
         // Generate the output
         let mut out = vec![];
-        out.extend(self.a.to_le_bytes().to_vec());
-        out.extend(self.b.to_le_bytes().to_vec());
-        out.extend(self.c.to_le_bytes().to_vec());
-        out.extend(self.d.to_le_bytes().to_vec());
+        out.extend(self.state[0].to_le_bytes().to_vec());
+        out.extend(self.state[1].to_le_bytes().to_vec());
+        out.extend(self.state[2].to_le_bytes().to_vec());
+        out.extend(self.state[3].to_le_bytes().to_vec());
 
         out
     }
